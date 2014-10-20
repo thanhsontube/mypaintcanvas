d1d84d3712d2625aef8752638c893b39d937fc57
nguyenthanhson.uit@gmail.com <nguyenthanhson.uit@gmail.com@dc767bfb-a31a-4c87-8a0b-7c924198874f>
2014-10-20 04:05:11 +0000

    test render: imageview to fix

--- trunk/TapcoPaint/src/com/example/tapcopaint/graphic2/MyRender.java
+++ trunk/TapcoPaint/src/com/example/tapcopaint/graphic2/MyRender.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
@@ -11,6 +12,7 @@ import android.widget.ImageView.ScaleType;
 
 import com.androidquery.AQuery;
 import com.example.tapcopaint.utils.FilterLog;
+import com.example.tapcopaint.zoom.ZoomVariables;
 
 public class MyRender extends SurfaceRenderer {
     private static final String TAG = "MyRender";
@@ -30,8 +32,6 @@ public class MyRender extends SurfaceRenderer {
 
     @Override
     public void start() {
-        // TODO Auto-generated method stub
-        log.d("log>>> " + "start");
 
     }
 
@@ -70,22 +70,125 @@ public class MyRender extends SurfaceRenderer {
     private final Rect canvasRect_ = new Rect(0, 0, 0, 0);
     private final Rect imageRect_ = new Rect(0, 0, 0, 0);
     private final Point dstSize_ = new Point();
+    private int canvasW, canvasH;
+    private int backgroundW, backgroundH;
+    BitmapFactory.Options options;
 
     /**
      * draw imagebackground
      */
     int i = 1;
+    public static final int VALUE_LOG = 5;
     Bitmap bitmap = null;
+    Matrix matrix;
+    private Matrix prevMatrix;
+    Paint paint;
+    private float[] m;
+    private ScaleType mScaleType;
+    private AQuery aQuery;
+    private boolean onDrawReady;
+    private ZoomVariables delayedZoomVariables;
 
     // bitmap = Bitmap.createBitmap(viewPort.getPhysicalWidth(), viewPort.getPhysicalHeight(),
     // Bitmap.Config.ARGB_8888);
 
     public void drawOriginalImage(Context context, int id, ViewPort viewPort) {
-        int canvasW = viewPort.getPhysicalWidth();
-        int canvasH = viewPort.getPhysicalHeight();
-        canvasRect_.set(0, 0, canvasW, canvasH);
+        aQuery = new AQuery(context);
+        i++;
+        canvasW = viewPort.getPhysicalWidth();
+        canvasH = viewPort.getPhysicalHeight();
 
-        final BitmapFactory.Options options = new BitmapFactory.Options();
+        if (i == VALUE_LOG) {
+            log.d("log>>> " + "canvasW :" + canvasW + ";canvasH:" + canvasH);
+        }
+        canvasRect_.set(0, 0, canvasW, canvasH);
+        paint = new Paint(Paint.FILTER_BITMAP_FLAG);
+        options = new BitmapFactory.Options();
+        m = new float[9];
+
+        // mScaleType = ScaleType.CENTER_INSIDE;
+        mScaleType = ScaleType.FIT_CENTER;
+        // mScaleType = ScaleType.MATRIX;
+
+        bitmap = aQuery.getCachedImage(id);
+        if (bitmap == null) {
+            if (i == VALUE_LOG) {
+                log.e("log>>> " + "drawOriginalImage Error: decode bitmap error id:" + id);
+            }
+            return;
+        }
+
+        backgroundW = bitmap.getWidth();
+        backgroundH = bitmap.getHeight();
+        if (i == VALUE_LOG) {
+            log.d("log>>> " + "backgroundW :" + backgroundW + ";backgroundH:" + backgroundH);
+        }
+
+        matrix = new Matrix();
+        prevMatrix = new Matrix();
+
+        drawMatrix(context, id, viewPort);
+    }
+
+    public void drawMatrix(Context context, int id, ViewPort viewPort) {
+        Canvas canvas = new Canvas(viewPort_.bitmap_);
+
+        matrix.getValues(m);
+        prevMatrix.setValues(m);
+
+        // scale image for view
+
+        float scaleX = (float) canvasW / backgroundW;
+        float scaleY = (float) canvasH / backgroundH;
+        switch (mScaleType) {
+        case CENTER:
+            scaleX = scaleY = 1;
+            break;
+
+        case CENTER_CROP:
+            scaleX = scaleY = Math.max(scaleX, scaleY);
+            break;
+
+        case CENTER_INSIDE:
+            scaleX = scaleY = Math.min(1, Math.min(scaleX, scaleY));
+
+        case FIT_CENTER:
+            scaleX = scaleY = Math.min(scaleX, scaleY);
+            break;
+
+        case FIT_XY:
+            break;
+        default:
+            break;
+        }
+
+        // matrix.setTranslate(-10, -40);
+        matrix.setScale(scaleX, scaleY);
+
+        // center the image
+        float paddingX = canvasW - (scaleX * backgroundW);
+        float paddingY = canvasH - (scaleY * backgroundH);
+
+        matrix.postTranslate(paddingX / 2, paddingY / 2);
+        canvas.drawBitmap(bitmap, matrix, paint);
+        onDrawReady = true;
+
+    }
+    
+    public void setZoom (float scale, float focusX, float focusY, ScaleType scaleType) {
+        if (!onDrawReady) {
+            delayedZoomVariables = new ZoomVariables(scale, focusX, focusY, scaleType);
+            return;
+        }
+        
+        if (scaleType != mScaleType) {
+            this.mScaleType = scaleType;
+        }
+        
+    }
+
+    public void drawCurrent(Context context, int id, ViewPort viewPort) {
+
         Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
 
         i++;
@@ -189,6 +292,7 @@ public class MyRender extends SurfaceRenderer {
         // Canvas canvas = new Canvas(viewPort.bitmap_);
         // Paint p = new Paint(Paint.FILTER_BITMAP_FLAG);
         // canvas.drawBitmap(bitmap, canvasRect_, imageRect_, null);
+
     }
 
     /**
@@ -196,7 +300,6 @@ public class MyRender extends SurfaceRenderer {
      * 
      */
     public void drawLayer(Context context, ViewPort viewPort) {
-
     }
 
 }
