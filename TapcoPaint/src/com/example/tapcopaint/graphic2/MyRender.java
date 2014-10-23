8c41e3c2dc074fa56c4e3ef1dc4d5af0c0507e09
nguyenthanhson.uit@gmail.com <nguyenthanhson.uit@gmail.com@dc767bfb-a31a-4c87-8a0b-7c924198874f>
2014-10-23 08:20:55 +0000

    zoom2

--- TapcoPaint/src/com/example/tapcopaint/graphic2/MyRender.java
+++ TapcoPaint/src/com/example/tapcopaint/graphic2/MyRender.java
@@ -13,10 +13,13 @@ import android.graphics.PointF;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.util.FloatMath;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView.ScaleType;
 
 import com.androidquery.AQuery;
+import com.example.tapcopaint.paint.TsPaint;
 import com.example.tapcopaint.utils.FilterLog;
 import com.example.tapcopaint.view.CommandManager;
 import com.example.tapcopaint.zoom.ZoomVariables;
@@ -51,6 +54,8 @@ public class MyRender extends SurfaceRenderer {
         maxScale = 3;
         superMinScale = SUPER_MIN_MULTIPLIER * minScale;
         superMaxScale = SUPER_MAX_MULTIPLIER * maxScale;
+        matrix = new Matrix();
+        m = new float[9];
 
     }
 
@@ -324,15 +329,99 @@ public class MyRender extends SurfaceRenderer {
         // matrix.postScale((float) deltaScale, (float) deltaScale, focusX, focusY);
 
         log.d("log>>> " + "focusX:" + focusX + ";focusY:" + focusY + ";deltaScale:" + deltaScale);
-
+        //
         scaleX *= (float) deltaScale;
         scaleY *= (float) deltaScale;
-        
-//        final float x = (float) ((viewPort_.getPhysicalWidth() - deltaScale)/deltaScale);
-//        final float y = (float) ((viewPort_.getPhysicalHeight() - deltaScale)/deltaScale);
-//        
-//        translateX = x - focusX + focusX; // canvas X
-//        translateY = y - focusY + focusY; // canvas Y
+        pointMidFinger.x = focusX;
+        pointMidFinger.y = focusY;
+
+        // move center
+
+        newW = canvasW * scaleX;
+        newH = canvasH * scaleY;
+
+        float tx = newW / 2;
+        float ty = newH / 2;
+
+        translateX = tx - pointMid.x;
+        translateY = ty - pointMid.y;
+
+        // newW = canvasW * scaleX;
+        // newH = canvasH * scaleY;
+        //
+        // float tx = focusX * scaleX;
+        // float ty = focusY * scaleY;
+        //
+        // translateX = tx - focusX;
+        // translateY = ty - focusY;
+
+        // matrix.getValues(m);
+        // float transX = m[Matrix.MTRANS_X];
+        // float transY = m[Matrix.MTRANS_Y];
+        //
+        // float fixTransX = getFixTrans(transX, canvasW, backgroundW);
+        // float fixTransY = getFixTrans(transY, canvasH, backgroundH);
+        //
+        // if (fixTransX != 0 || fixTransY != 0) {
+        // translateX = fixTransX;
+        // translateY = fixTransY;
+        // }
+
+        float[] fa = getAbsolutePosition(pointMid.x, pointMid.y, focusX, focusY);
+
+        screenSize = new PointF(viewPort_.bitmap_.getWidth(), this.viewPort_.bitmap_.getHeight());
+        sceneSize = new PointF(getBackgroundSize());
+        // sceneSize = new PointF(fa[0], fa[1]);
+
+        sceneSize.x = getAbsolutePosition(pointMid.x, fa[0], scaleX);
+        sceneSize.y = getAbsolutePosition(pointMid.y, fa[1], scaleY);
+
+        // translateX = screenSize.x / 2;
+        // translateY = screenSize.y / 2;
+
+        float screenWidthToHeight = screenSize.x / screenSize.y;
+        float screenHeightToWidth = screenSize.y / screenSize.x;
+
+        RectF w1 = new RectF(this.viewPort_.window);
+        RectF w2 = new RectF();
+        PointF sceneFocus = new PointF(w1.left + (focusX / screenSize.x) * w1.width(), w1.top + (focusY / screenSize.y)
+                * w1.height());
+
+        float w2Width = viewPort_.getPhysicalWidth() * scaleX;
+        float w2Height = w2Width * screenHeightToWidth;
+
+        w2.left = sceneFocus.x - ((focusX / screenSize.x) * w2Width);
+        w2.top = sceneFocus.y - ((focusY / screenSize.y) * w2Height);
+        if (w2.left < 0)
+            w2.left = 0;
+        if (w2.top < 0)
+            w2.top = 0;
+        w2.right = w2.left + w2Width;
+        w2.bottom = w2.top + w2Height;
+        if (w2.right > sceneSize.x) {
+            w2.right = sceneSize.x;
+            w2.left = w2.right - w2Width;
+        }
+        if (w2.bottom > sceneSize.y) {
+            w2.bottom = sceneSize.y;
+            w2.top = w2.bottom - w2Height;
+        }
+
+        // pointMidFinger.x = w2.left;
+        // pointMidFinger.y = w2.top;
+
+        // Point p1 = new Point();
+        // viewPort_.getOrigin(p1);
+        // translateX = p1.x;
+        // translateY = p1.y;
+
+        // midPoint(pointMidFinger, focusX, focusY);
+
+        // final float x = (float) ((viewPort_.getPhysicalWidth() - deltaScale)/deltaScale);
+        // final float y = (float) ((viewPort_.getPhysicalHeight() - deltaScale)/deltaScale);
+        //
+        // translateX = x - focusX + focusX; // canvas X
+        // translateY = y - focusY + focusY; // canvas Y
 
         // translateX = focusX;
         // translateY = focusY;
@@ -497,19 +586,39 @@ public class MyRender extends SurfaceRenderer {
 
     float mScaleFactor = 1;
 
+    PointF pointMidFinger = new PointF();
+    PointF pointMid = new PointF();
+
+    PointF screenSize = new PointF();
+    PointF sceneSize = new PointF();
+
+    Point pA1 = new Point();
+
+    float newW, newH;
+
     public void drawRect(ViewPort viewPort) {
+        Canvas canvas = new Canvas(viewPort_.bitmap_);
+
+        backgroundW = viewPort_.bitmap_.getWidth();
+        backgroundH = viewPort_.bitmap_.getHeight();
+
+        canvasW = viewPort_.getPhysicalWidth();
+        canvasH = viewPort_.getPhysicalHeight();
+        matrix.getValues(m);
+        pointMid = new PointF(viewPort_.getPhysicalWidth() / 2, viewPort_.getPhysicalHeight() / 2);
 
         Point p = new Point();
         viewPort.getSize(p);
 
         // log.d("log>>> " + "Viewport X:" + p.x + ";y:" + p.y);
-        Canvas canvas = new Canvas(viewPort_.bitmap_);
 
         RectF rectF = new RectF(100, 100, 300, 500);
 
         canvas.drawColor(Color.WHITE);
-        canvas.translate(translateX, translateY);
+
         canvas.scale(scaleX, scaleY);
+
+        canvas.translate(-translateX / scaleX, -translateY / scaleY);
 
         Paint pCircle = resetPaint();
         pCircle.setColor(Color.GREEN);
@@ -524,14 +633,9 @@ public class MyRender extends SurfaceRenderer {
 
         canvas.drawPoint(550, 300, resetPaint());
 
-        canvas.drawCircle(550, 300, r, pCircle);
+        canvas.drawCircle(canvasW / 2, canvasH / 2, r, pCircle);
 
         canvas.drawRect(rectF, resetPaint());
-
-        // finger point 1
-        canvas.drawCircle(550, 100, 50, pCircle);
-        // finger point 2
-        canvas.drawCircle(550, 500, 50, pCircle);
 
         // draw line
         Path p1 = new Path();
@@ -543,22 +647,108 @@ public class MyRender extends SurfaceRenderer {
 
         canvas.drawPath(p1, pPath);
 
+        // draw mid point
+        canvas.drawCircle(pointMidFinger.x, pointMidFinger.y, 25, TsPaint.getBlackPaint());
+
+        // screen size
+
+        canvas.drawCircle(screenSize.x, screenSize.y, 25, TsPaint.getRedPaint());
+
+        // scenesize
+        canvas.drawCircle(sceneSize.x, sceneSize.y, 25, TsPaint.getGreenPaint());
+
+        canvas.drawCircle(pointMid.x, pointMid.y, 25, TsPaint.getBluePaint());
+
+        // a1
+        pA1.x = 100;
+        pA1.y = 100;
+        canvas.drawCircle(pA1.x, pA1.y, 25, TsPaint.getRedPaint());
+
+        // text
+        float spaceX = (pointMid.x - pA1.x) / scaleX;
+        float spaceY = (pointMid.y - pA1.y) / scaleY;
+
+        float edgesX = screenSize.x - pointMid.x;
+
+        float canvasX = viewPort_.getPhysicalWidth() - spaceX - edgesX;
+        String text = "space x:" + spaceX + ";canvasX :" + canvasX;
+
+        canvas.drawText(text, 0, 50, TsPaint.getRedPaint(1));
+
     }
 
-    public float[] getAbsolutePosition(float Ax, float Ay, float Bx, float By) {
+    public float[] getAbsolutePosition(float Ax, float Ay, float mCenterScaleX, float mCenterScaleY) {
 
         Point p = new Point();
         viewPort_.getSize(p);
 
-        float fromAxToBxInCanvasSpace = (translateX - Ax) / mScaleFactor;
-        float fromBxToCanvasEdge = p.x - Bx;
+        float fromAxToBxInCanvasSpace = (mCenterScaleX - Ax) / mScaleFactor;
+        float fromBxToCanvasEdge = p.x - pointMid.x;
         float x = p.x - fromAxToBxInCanvasSpace - fromBxToCanvasEdge;
 
-        float fromAyToByInCanvasSpace = (p.y - Ay) / mScaleFactor;
-        float fromByToCanvasEdge = p.y - By;
+        float fromAyToByInCanvasSpace = (mCenterScaleY - Ay) / mScaleFactor;
+        float fromByToCanvasEdge = p.y - pointMid.y;
         float y = p.y - fromAyToByInCanvasSpace - fromByToCanvasEdge;
 
         return new float[] { x, y };
+    }
+
+    /** Determine the space between the first two fingers */
+    public float spacing(float x, float y) {
+        return FloatMath.sqrt(x * x + y * y);
+    }
+
+    /** Calculate the mid point of the first two fingers */
+    public void midPoint(PointF point, float x, float y) {
+        point.set(x / 2, y / 2);
+    }
+
+    public float getAbsolutePosition(float oldCenter, float newCenter, float mScaleFactor) {
+        if (newCenter > oldCenter) {
+            return oldCenter + ((newCenter - oldCenter) / mScaleFactor);
+        } else {
+            return oldCenter - ((oldCenter - newCenter) / mScaleFactor);
+        }
+    }
+
+    private void fixTrans() {
+        matrix.getValues(m);
+        float transX = m[Matrix.MTRANS_X];
+        float transY = m[Matrix.MTRANS_Y];
+
+        float fixTransX = getFixTrans(transX, canvasW, backgroundW);
+        float fixTransY = getFixTrans(transY, canvasH, backgroundH);
+
+        if (fixTransX != 0 || fixTransY != 0) {
+            matrix.postTranslate(fixTransX, fixTransY);
+        }
+    }
+
+    private float getFixTrans(float trans, float viewSize, float contentSize) {
+        float minTrans, maxTrans;
+
+        if (contentSize <= viewSize) {
+            minTrans = 0;
+            maxTrans = viewSize - contentSize;
+
+        } else {
+            minTrans = viewSize - contentSize;
+            maxTrans = 0;
+        }
+
+        if (trans < minTrans)
+            return -trans + minTrans;
+        if (trans > maxTrans)
+            return -trans + maxTrans;
+        return 0;
+    }
+
+    public void onMove(float x, float y) {
+        synchronized (this) {
+
+            translateX = -x / scaleX;
+            translateY = -y / scaleY;
+        }
     }
 
 }
